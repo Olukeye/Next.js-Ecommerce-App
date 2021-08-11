e6a42eb29c2d2b2d3a8fb33c70eac7b9f0c3bc91
Olukeye <seunolukeye2000@gmail.com>
2021-08-11 23:55:13 +0100

    edited socials

--- src/core/Footer.js
+++ src/core/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import './footer.css'
 
 const Footer = () => {
@@ -10,9 +11,9 @@ const Footer = () => {
             ultricies feugiat leo. Suspendisse tellus elit, pharetra in hendrerit ut, aliquam quis augue. Nam ut nibh mollis, tristique ante sed, viverra massa.
           </p>
           <div className="icons">
-            <a href="#"><i className="fa fa-facebook"></i></a>
-            <a href="#"><i className="fa fa-twitter"></i></a>
-            <a href="#"><i className="fa fa-linkedin"></i></a>
+            <a href="https://github.com/Olukeye"><i className="fa fa-github"></i></a>
+            <a href="https://twitter.com/Tyjani3"><i className="fa fa-twitter"></i></a>
+            <a href="https://www.linkedin.com/in/seun-olukeye-9a1b26128/"><i className="fa fa-linkedin"></i></a>
             <a href="#"><i className="fa fa-google-plus"></i></a>
             <a href="#"><i className="fa fa-instagram"></i></a>
           </div>
@@ -20,25 +21,25 @@ const Footer = () => {
         <div className="footer-center col-md-4 col-sm-6">
           <div>
             <i className="fa fa-map-marker"></i>
-            <p><span> Street name and number</span> City, Country</p>
+            <p><span> Street name and number</span> Ifako Ijaiye, Lagos</p>
           </div>
           <div>
             <i className="fa fa-phone"></i>
-            <p> (+00) 0000 000 000</p>
+            <p> (+234) 080 3770 5238 </p>
           </div>
           <div>
             <i className="fa fa-envelope"></i>
-            <p><a href="#"> office@company.com</a></p>
+            <p><a href="seunolukeye2000@gmail.com">seunolukeye@gmail.com</a></p>
           </div>
         </div>
         <div className="footer-right col-md-4 col-sm-6">
           <h2> Company<span> logo</span></h2>
           <p className="menu">
-            <a href="#"> Home</a> |
+            <a href="/"> Home</a> |
             <a href="#"> About</a> |
             <a href="#"> Services</a> |
             <a href="#"> Portfolio</a> |
-            <a href="#"> News</a> |
+            <a href="/shop">Shop</a> |
             <a href="#"> Contact</a>
           </p>
           <p className="name"> Company Name &copy; 2016</p>
